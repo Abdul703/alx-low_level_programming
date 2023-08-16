398a40bbdeb6133889c9b0007baff36767914a78
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-08-15 18:14:23 -0700

    removed main function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,24 +2,13 @@
 #include "main.h"
 
 /**
- * _putchar - print character to stdout
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * @c: character to print
- *
- * Return: always char as success
- * ..
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
-}
-
-/**
- * main - entry point
- * Return: 0 for success
- */
-int main(void)
-{
-_putchar('c');
-return (0);
+	return (write(1, &c, 1));
 }
