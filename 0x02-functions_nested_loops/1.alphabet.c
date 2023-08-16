b7d080db8db201a03a970ed7a2f6ba3bae785661
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-08-15 19:08:17 -0700

    added function to print alphabet

---
+++ 0x02-functions_nested_loops/1.alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all lower case alphabet a-z
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+for (i = 'a'; i <= 'z' i++)
+{
+	_putchar(i);
+}
+putchar('\n')
+}
