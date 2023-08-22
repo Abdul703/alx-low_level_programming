064e4f66d62d4216b64e1ef3d8380103e9e922ed
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-08-22 13:59:53 -0700

    added function that print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - printing the last half of a string
+ *
+ * @str: string to print last half
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+
+int length_of_the_string, n;
+char *str_copy = str;
+
+/* getting the length of the string */
+while (*str_copy != '\0')
+{
+	str_copy++;
+	length_of_the_string++;
+}
+length_of_the_string--;
+
+/* compute the half of the string */
+if (length_of_the_string % 2 == 0)
+	n = length_of_the_string / 2;
+else
+	n = (length_of_the_string - 1) / 2;
+
+/* printing the last half*/
+while (n < length_of_the_string)
+{
+	_putchar(*(str + n));
+	n++;
+}
+_putchar('\n');
+}
