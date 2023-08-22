d41bed5f46e7dfa3f14ac0c951798d343e492f5b
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-08-22 15:45:10 -0700

    added function that copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string pointed to a variable to another variable
+ *
+ * @src: the sring pointer to copy
+ * @dest: the string pointer to copy to
+ *
+ * Return: pointer to @dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (*src != '\0')
+{
+	dest[i] = *src;
+
+	i++;
+	src++;
+}
+
+return (dest);
+}
