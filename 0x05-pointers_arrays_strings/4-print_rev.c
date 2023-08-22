646845c9afddea9aeac34da3a593f47d9d2ceff9
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-08-21 18:43:18 -0700

    added fuction that reverse a string without the use of std libraries

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - printing string in reverse
+ *
+ * @s: the string to reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+char *first_char_location = s;
+
+/* getting the last character */
+while (*s != '\0')
+{
+	s++;
+}
+
+/* printing the string in reverse */
+while (s != first_char_location)
+{
+	_putchar(*s);
+	--s;
+}
+_putchar('\n');
+}
