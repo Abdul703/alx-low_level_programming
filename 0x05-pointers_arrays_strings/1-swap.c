0803cd3c971e86e8bbb54fce5d07ddf4546e95df
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-08-21 17:27:03 -0700

    added function that swap integers using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swap 2 integers using pointer
+ *
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int initial_a;
+
+initial_a = *a;
+*a = *b;
+*b = initial_a;
+}
