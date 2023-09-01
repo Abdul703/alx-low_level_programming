bbb871afb26311a2beb25b59b263ce7e22e8ccbb
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-01 13:20:45 -0700

    fixed betty styling

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - prints name of the program
+ *
+ * @argc: count arguments
+ * @argv: string arguments
+ *
+ * Return: always 0 for success
+ */
+int main(int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
