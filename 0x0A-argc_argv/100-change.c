441bc2265be2c29bd6bebb5fb4dca19f2408e6df
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-01 17:40:24 -0700

    handle bad argument type argument

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * isStringDigit - check if string is digit
+ *
+ * @str: string to check
+ *
+ * Return: bool
+ */
+bool isStringDigit(char *str)
+{
+while (*str != '\0')
+{
+	if (!isdigit(*str))
+	{
+		return (false);
+	}
+	str++;
+}
+return (true);
+}
 
 /**
  * getChange - prints the minimum number of coins to
@@ -36,10 +58,11 @@ return (count);
  */
 int main(int argc, char *argv[])
 {
-int cents = atoi(argv[1]);
 
-if (argc == 2)
+if (argc == 2 && isStringDigit(argv[1]))
 {
+	int cents = atoi(argv[1]);
+
 	printf("%d\n", getChange(cents));
 }
 else
