5a6e6b375a5ec5b9e4c5aeaf1598bcae6c4693f1
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-07 07:23:46 -0700

    cast pointer from void to char

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,9 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	void *ptr;
+	char *byte_ptr;
+	unsigned int i;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
@@ -20,7 +22,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
-	*ptr = 0;
+	byte_ptr = (char *)ptr;
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		byte_ptr[i] = 0;
+	}
 
 	return (ptr);
 }
