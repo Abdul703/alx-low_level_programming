c17dbe010eaee5a8d3cf48171623b6c58af96213
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-07 09:01:02 -0700

    added function that create arrays of range min to max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,3 +1,33 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stddef.h>
+
+/**
+ * array_range - creates an array of integers given minimum and maximum value
+ *
+ * @min: minimum value
+ * @max: maximum value
+ *
+ * Return: pointer to the range of arrays
+ */
+int *array_range(int min, int max)
+{
+	int size, *ptr;
+
+	/* check if min value is not higher, then compute the size */
+	if (min > max)
+		return (NULL);
+	size = (max - min + 1) * sizeof(int);
+
+	/* allocate memory */
+	ptr = malloc(size);
+	if (ptr == NULL)
+		return (NULL);
+
+	/* initialize values */
+	for (; min <= max; min++)
+	{
+		ptr[min] = min;
+	}
+	return (ptr);
+}
