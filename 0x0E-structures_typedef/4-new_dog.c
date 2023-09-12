09b40cc28b7ab1ead5acbf8674f19409245979a8
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-12 16:53:22 -0700

    fixed wrong output

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,23 +19,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = strdup(name);
+	dog->name = malloc(strlen(name) + 1);
 	if (dog->name == NULL)
 	{
-		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
+	strcpy(dog->name, name);
 
 	dog->age = age;
 
-	dog->owner = strdup(owner);
+	dog->owner = malloc(strlen(owner) + 1);
 	if (dog->owner == NULL)
 	{
-		free(dog->owner);
 		free(dog);
 		return (NULL);
 	}
+	strcpy(dog->owner, owner);
 
 	return (dog);
 }
