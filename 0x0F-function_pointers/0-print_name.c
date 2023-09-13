edaa6133642e73e97cfa02e90a3653b1f5775215
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-13 14:01:58 -0700

    added condition to check if calling function is not null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - prints a name
@@ -10,5 +11,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL)
+	{
+		f(name);
+	}
 }
