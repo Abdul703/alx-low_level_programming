c84b2ee1a88f4862b06076b50f8fc195729868b3
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-14 15:10:05 -0700

    renamed folder

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all the function parameters
+ *
+ * @n: number of passed arguments
+ *
+ * Return: total sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0, value;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(args, int);
+		sum += value;
+	}
+	return (sum);
+}
