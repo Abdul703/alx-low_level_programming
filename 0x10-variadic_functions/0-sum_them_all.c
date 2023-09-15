a43c7a56e39e3be1b0455d4a0937585110e346cd
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-14 17:27:09 -0700

    fixed betty style error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,29 +1,29 @@
 #include "variadic_functions.h"
-#include <stdarg.h> 
- 
-/** 
- * sum_them_all - sum all the function parameters 
- * 
- * @n: number of passed arguments 
- * 
- * Return: total sum 
- */ 
-int sum_them_all(const unsigned int n, ...) 
-{ 
-        unsigned int i; 
-        int sum = 0, value; 
-        va_list args; 
- 
-        if (n == 0) 
-                return (0); 
- 
-        va_start(args, n); 
- 
-        for (i = 0; i < n; i++) 
-        { 
-                value = va_arg(args, int); 
-                sum += value; 
-        } 
-        va_end(args); 
-        return (sum); 
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all the function parameters
+ *
+ * @n: number of passed arguments
+ *
+ * Return: total sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0, value;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(args, int);
+		sum += value;
+	}
+	va_end(args);
+	return (sum);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 			printf("%s", str);
 			if (separator != NULL && i + 1 != n)
-				printf("%s ", separator);
+				printf("%s", separator);
 		}
 		printf("\n");
 	}
