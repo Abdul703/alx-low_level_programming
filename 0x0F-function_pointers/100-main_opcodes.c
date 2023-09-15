1e1ab4f8cfb243497f2787e61089a93254f52f2c
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-15 06:55:03 -0700

    added function that print the opcodes of the program

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the opcodes of the main function
+ *
+ * @argc: Number of arguments
+ * @argv: Pointer to the arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	char *mainPtr;
+	int i, number_of_bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	mainPtr = (char *)&main;
+	number_of_bytes = atoi(argv[1]);
+
+	for (i = 0; i < number_of_bytes; i++)
+	{
+		printf("%02hhx ", mainPtr[i]);
+	}
+
+	printf("\n");
+	return (0);
+}
