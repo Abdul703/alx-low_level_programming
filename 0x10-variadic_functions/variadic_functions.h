e6a984214b7bf5cfe803c6a306360809658f0856
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-14 17:43:59 -0700

    fixed wrong spelling

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADC_FUNCTION_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 char _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
