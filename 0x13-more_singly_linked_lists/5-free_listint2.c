de4d9779a149e8798c471bdecf5537c0062f252b
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-26 03:58:58 -0700

    added function that frees the link list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * free_listint2 - freed linked list from memory and set the head to null
+ *
+ * @head: the linked list to free
+ *
+ * Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head, *next;
+
+	if (*head == NULL || head == NULL)
+		return;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
