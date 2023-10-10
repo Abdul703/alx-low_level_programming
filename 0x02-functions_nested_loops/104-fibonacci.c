1d86e67b8b3688d6e71f7d4ecafd011ca71ce828
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-10-10 04:52:57 -0700

    added function that print 98 fibonacci sequence

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * print_fibonacci - print fibonacci sequence starting from 1, 2, till the 50th
+ *
+ * @first: the starting number
+ * @second: the second number
+ * @count: initial count
+ *
+ * Return: void
+*/
+void print_fibonacci(long int first, long int second, int count)
+{
+	printf("%lu", first);
+	if (count == 98)
+	{
+		printf("\n");
+		return;
+	}
+	printf(", ");
+
+	count++;
+	print_fibonacci(second, first + second, count);
+}
+
+/**
+ * main - main entry
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+	print_fibonacci(1, 2, 1);
+	return (0);
+}
