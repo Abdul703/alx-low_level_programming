ee5cc94d0e2b9cba4ef6bcbd290537e93090aa47
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-10-31 16:46:05 -0700

    added function that copy file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+        exit(97);
+    }
+
+    copy_file(argv[1], argv[2]);
+
+    return (0);
+}
+
+void copy_file(const char *from_file, const char *to_file)
+{
+    int from_fd, to_fd;
+    ssize_t bytes_read;
+    char buffer[BUFSIZE];
+
+    from_fd = open(from_file, O_RDONLY);
+    if (from_fd == -1)
+    {
+        dprintf(2, "Error: Can't read from file %s\n", from_file);
+        exit(98);
+    }
+
+    to_fd = open(to_file, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (to_fd == -1)
+    {
+        dprintf(2, "Error: Can't write to file %s\n", to_file);
+        exit(99);
+    }
+
+    while ((bytes_read = read(from_fd, buffer, BUFSIZE)) > 0)
+    {
+        if (write(to_fd, buffer, bytes_read) == -1)
+	{
+            dprintf(2, "Error: Can't write to file %s\n", to_file);
+            exit(99);
+        }
+    }
+
+    if (bytes_read == -1)
+    {
+        dprintf(2, "Error: Can't read from file %s\n", from_file);
+        exit(98);
+    }
+
+    if (close(from_fd) == -1 || close(to_fd) == -1)
+    {
+        dprintf(2, "Error: Can't close fd %d\n", from_fd);
+        exit(100);
+    }
+}
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFSIZE 1024
+
+void copy_file(const char *from_file, const char *to_file);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
