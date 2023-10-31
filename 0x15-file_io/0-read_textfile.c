e5a25c6639d46081899b1fb6e930e396c842d2be
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-10-31 15:16:08 -0700

    fixed incorrect output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -37,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	buffer[bytesRead] = '\0';
-	printf("%s", buffer);
+	fprintf(stderr, "%s", buffer);
 
 	free(buffer);
 	fclose(file);
