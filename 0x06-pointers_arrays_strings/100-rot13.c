286122c662b689dcac4469e0be9007160e993bd1
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2024-02-03 02:15:54 +0100

    fixed style issue

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- * rot13 - encode leat letters on a string.
+ * rot13 - rotate letters by 13.
  *
- * @str: string to encode
+ * @str: string to rotate
  *
  * Return: pointer to the result string
  */
 char *rot13(char *str)
 {
 	int i, j;
-    char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-        for (j = 0; input[j] != '\0'; j++)
-        {
-            if (str[i] == input[j])
-            {
-                *(str + i) = output[j];
-                break;
-            }
-            
-        }
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				*(str + i) = output[j];
+				break;
+			}
+		}
 	}
 	return (str);
 }
